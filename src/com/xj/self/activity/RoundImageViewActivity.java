fd1ce4fef69b398378df0fbd55e1d54c2fff710f
xj <xiongjin2013@126.com>
2015-06-19 20:21:25 +0800

    imageview圆角图片控件实现--RoundImageView

---
+++ src/com/xj/self/activity/RoundImageViewActivity.java
@@ -0,0 +1,16 @@
+package com.xj.self.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.haha.self.R;
+
+public class RoundImageViewActivity extends ActionBarActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_round_image);
+    }
+
+}
