9c9d8fcb207f8b57ee89a1bf6a9a0c3b72b048d1
xj <xiongjin2013@126.com>
2015-08-18 18:52:01 +0800

    add onClickListener for custom view

--- src/com/xj/self/view/TitleView.java
+++ src/com/xj/self/view/TitleView.java
@@ -1,6 +1,10 @@
 
 package com.xj.self.view;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -9,15 +13,13 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.TextView;
 
 import com.haha.self.R;
 
 /**
- * the first custom view example 自定义view的步骤如下：
- *  1.自定义view的属性； 
- *  2.在View的构造方法中获取我们自定义的属性 
- *  3.重写onMeasure
+ * the first custom view example 自定义view的步骤如下： 1.自定义view的属性； 2.在View的构造方法中获取我们自定义的属性 3.重写onMeasure
  * //非必须，但一般都要重写 4.重写onDraw
  * 
  * @author xj
@@ -106,47 +108,72 @@ public class TitleView extends TextView {
         mBound = new Rect();
         mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound); // 设置绘制文本的范围：宽和高；
 
+        /**
+         * 设置点击的监听事件 让它随机生成一个4位的随机数， 有兴趣的可以在onDraw中添加一点噪点，然后改写为验证码
+         */
+        setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mTitleText = randomText();
+                postInvalidate();
+            }
+        });
+    }
+
+    private String randomText() {
+        Random random = new Random();
+        Set<Integer> set = new HashSet<Integer>();
+        while (set.size() < 4) {
+            int randomInt = random.nextInt(10);
+            set.add(randomInt);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (Integer i : set) {
+            sb.append(i);
+        }
+
+        return sb.toString();
     }
 
     /**
-     * 重写之前先了解MeasureSpec的specMode,一共三种类型： 
-     * EXACTLY：一般是设置了明确的值或者是MATCH_PARENT
-     * AT_MOST：表示子布局限制在一个最大值内，一般为WARP_CONTENT 
-     * UNSPECIFIED：表示子布局想要多大就多大，很少使用
+     * 重写之前先了解MeasureSpec的specMode,一共三种类型： EXACTLY：一般是设置了明确的值或者是MATCH_PARENT
+     * AT_MOST：表示子布局限制在一个最大值内，一般为WARP_CONTENT UNSPECIFIED：表示子布局想要多大就多大，很少使用
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        
+
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        
+
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-        
+
         int width = 0;
         int height = 0;
-        
-        if(widthMode == MeasureSpec.EXACTLY){ 
+
+        if (widthMode == MeasureSpec.EXACTLY) {
             width = widthSize;
-        }else{
+        } else {
             mPaint.setTextSize(mTitleTextSize);
             mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
             float textWidth = mBound.width();
-            int desired = (int)(getPaddingLeft()+textWidth+getPaddingRight());
+            int desired = (int) (getPaddingLeft() + textWidth + getPaddingRight());
             width = desired;
         }
-        
-        if(heightMode == MeasureSpec.EXACTLY){
+
+        if (heightMode == MeasureSpec.EXACTLY) {
             height = heightSize;
-        }else{
+        } else {
             mPaint.setTextSize(mTitleTextSize);
             mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
             float textHeight = mBound.height();
-            int desired = (int)(getPaddingTop()+textHeight+getPaddingBottom());
+            int desired = (int) (getPaddingTop() + textHeight + getPaddingBottom());
             height = desired;
         }
-        
-        setMeasuredDimension(width, height); //设置该自定义view的宽和高；
+
+        setMeasuredDimension(width, height); // 设置该自定义view的宽和高；
     }
 
     @Override
