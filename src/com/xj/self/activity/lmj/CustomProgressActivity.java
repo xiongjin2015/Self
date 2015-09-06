2e67f09fedcfb2b3002a61a39a50042c7a9c3bbd
xj <xiongjin2013@126.com>
2015-09-06 18:11:03 +0800

    custom circle progressbar

---
+++ src/com/xj/self/activity/lmj/CustomProgressActivity.java
@@ -0,0 +1,15 @@
+package com.xj.self.activity.lmj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.haha.self.R;
+
+public class CustomProgressActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_progress);
+    }
+}
