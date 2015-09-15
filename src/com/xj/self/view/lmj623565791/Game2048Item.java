ba5814086b3da40733c4b280b6ee89ca2e817add
xj <xiongjin2013@126.com>
2015-09-15 19:19:30 +0800

    2048 Game picture version

--- src/com/xj/self/view/lmj623565791/Game2048Item.java
+++ src/com/xj/self/view/lmj623565791/Game2048Item.java
@@ -1,34 +1,58 @@
+
 package com.xj.self.view.lmj623565791;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.Paint.Style;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.xj.self.R;
+
 /**
  * 2048的每个Item
- *
  */
 public class Game2048Item extends View {
-    
-    /**该View上的数字*/
+
+    /** 该View上的数字 */
     private int number;
     private String numberVal;
     private Paint paint;
-    
-    /**绘制文字的区域*/
+
+    /** 绘制文字的区域 */
     private Rect bound;
+    
+    private static int[] mImgs = new int[] { R.drawable.d0, R.drawable.d2,
+        R.drawable.d3, R.drawable.d4, R.drawable.d5, R.drawable.d6,
+        R.drawable.d7, R.drawable.d8, R.drawable.d9, R.drawable.d10,
+        R.drawable.d11 };
+
+    private static Bitmap[] mBitmaps = null;
+    {
+        if (mBitmaps == null)
+        {
+            mBitmaps = new Bitmap[mImgs.length];
+            for (int i = 0; i < mImgs.length; i++)
+            {
+                mBitmaps[i] = BitmapFactory.decodeResource(getResources(),
+                        mImgs[i]);
+            }
+    
+        }
+    
+    }
 
     public Game2048Item(Context context) {
-        this(context,null);
+        this(context, null);
     }
-    
+
     public Game2048Item(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public Game2048Item(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -36,23 +60,26 @@ public class Game2048Item extends View {
         paint = new Paint();
     }
 
-    public void setNumber(int number){
+    public void setNumber(int number) {
         this.number = number;
-        numberVal = number+"";
+        numberVal = number + "";
         paint.setTextSize(30.0f);
         bound = new Rect();
         paint.getTextBounds(numberVal, 0, numberVal.length(), bound);
         invalidate();
     }
-    
-    public int getNumber(){
+
+    public int getNumber() {
         return number;
     }
-    
+
+    /**
+     * 数字版
+     */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        
+
         String bgColor = "";
         switch (number) {
             case 0:
@@ -90,24 +117,78 @@ public class Game2048Item extends View {
                 break;
             case 2048:
                 bgColor = "#EA7821";
-                break;              
+                break;
             default:
                 bgColor = "#EA7821";
                 break;
         }
-        
+
         paint.setColor(Color.parseColor(bgColor));
         paint.setStyle(Style.FILL);
         canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
-        if(number != 0){
+        if (number != 0) {
             drawText(canvas);
         }
     }
-    
-    private void drawText(Canvas canvas){
+
+    /**
+     * 图片版
+     */
+//    @Override
+//    protected void onDraw(Canvas canvas){
+//
+//        super.onDraw(canvas);
+//        int index = -1;
+//        switch (number){
+//            case 2:
+//                index = 0;
+//                break;
+//            case 4:
+//                index = 1;
+//                break;
+//            case 8:
+//                index = 2;
+//                break;
+//            case 16:
+//                index = 3;
+//                break;
+//            case 32:
+//                index = 4;
+//                break;
+//            case 64:
+//                index = 5;
+//                break;
+//            case 128:
+//                index = 6;
+//                break;
+//            case 256:
+//                index = 7;
+//                break;
+//            case 512:
+//                index = 8;
+//                break;
+//            case 1024:
+//                index = 9;
+//                break;
+//            case 2048:
+//                index = 10;
+//                break;
+//        }
+//        if (number == 0){
+//            paint.setColor(Color.parseColor("#EEE4DA"));
+//            paint.setStyle(Style.FILL);
+//            canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+//        }
+//        //
+//        if (number != 0)
+//            canvas.drawBitmap(mBitmaps[index], null, new Rect(0, 0, getWidth(),
+//                    getHeight()), null);
+//    }
+
+    private void drawText(Canvas canvas) {
         paint.setColor(Color.BLACK);
-        float x = (getWidth() - bound.width())/2;
-        float y = getHeight() / 2 + bound.height()/2;
+        float x = (getWidth() - bound.width()) / 2;
+        float y = getHeight() / 2 + bound.height() / 2;
         canvas.drawText(numberVal, x, y, paint);
     }
 }
